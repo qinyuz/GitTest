800e2f515c9e81a88643286a2e4a8eb2fb838724
zhangqingyu <zhangqy@tenwa.com.cn>
2019-04-04 12:14:35 +0800

    com/tenwa/zqy/Test.java:8
    Signed-off-by: zhangqingyu <zhangqy@tenwa.com.cn>

--- src/com/tenwa/zqy/Test.java
+++ src/com/tenwa/zqy/Test.java
@@ -4,7 +4,10 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
+		System.out.println("各回各家韩庚韩庚韩庚结果");
 	}
 
 }
